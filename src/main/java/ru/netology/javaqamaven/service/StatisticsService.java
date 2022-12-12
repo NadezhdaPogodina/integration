1ec53851b0546264d57c85240e2ba2d7064d1bb8
NadezhdaPogodina <pogodinadia@inbox.ru>
2022-12-12 21:03:39 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqamaven/service/StatisticsService.java
@@ -0,0 +1,17 @@
+package ru.netology.javaqamaven.service;
+public class StatisticsService {
+    public int findMax(int[] incomes) {
+
+        int currentMaxIndex = 0;
+        int currentMax = incomes[0];
+
+        for (int i = 0; i < incomes.length; i++) ;
+        if (currentMax > currentMaxIndex) {
+            currentMaxIndex = currentMax;
+
+
+        }  return currentMaxIndex;
+
+    }
+}
+
